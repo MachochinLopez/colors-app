f7536e9d5d4bfdb9e69a803a5fd6f45658907a5d
Oscar López <machochinnio@hotmail.com>
2020-12-28 13:42:48 -0600

    Se agregó la clase ColorBox y sus estilos

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,12 +1,18 @@
 import { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map(color => {
+      return <ColorBox background={color.color} name={color.name} />;
+    });
+
     return (
       <div className="Palette">
         {/* Navbar goes here */}
         <div className="Palette-colors">
-          {/* Color boxes */}
+          { colorBoxes }
         </div>
         {/* Footer */}
       </div>
