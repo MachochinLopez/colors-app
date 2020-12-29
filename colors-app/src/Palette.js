053f2265d3025b08bba034055c9a2ecba6f48a08
Oscar López <machochinnio@hotmail.com>
2020-12-29 16:24:17 -0600

    Snackbar

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -6,25 +6,34 @@ import './Palette.css';
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeColorFormat = this.changeColorFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
   }
 
+  changeColorFormat(val) {
+    this.setState({ format: val });
+  }
+
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => {
-      const { name, hex } = color;
-      return <ColorBox key={name} background={hex} name={name} />;
+      const { name } = color;
+      return <ColorBox key={name} background={color[format]} name={name} />;
     });
 
     return (
       <div className="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeColorFormat}
+        />
         {/* Navbar goes here */}
         <div className="Palette-colors">
           { colorBoxes }
