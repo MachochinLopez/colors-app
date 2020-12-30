3a72e37b7339e1ea0c51b58662d01ea5032e9d07
Oscar López <machochinnio@hotmail.com>
2020-12-29 23:17:58 -0600

    Refactor de estilos pt 1

--- colors-app/src/SingleColorPalette.js
+++ colors-app/src/SingleColorPalette.js
@@ -4,48 +4,7 @@ import PaletteFooter from './PaletteFooter';
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    overflow: "hidden"
-  },
-  colors: {
-    height: "90%"
-  },
-  goBack: {
-    backgroundColor: "black",
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-4px",
-    opacity: "1",
-    "& a": {
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15px",
-      textAlign: "center",
-      outline: "none",
-      background: "rgba(255, 255, 255, 0.3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      color: "white",
-      textTransform: "uppercase",
-      border: "none",
-      textDecoration: "none"
-    }
-  }
-};
+import styles from './styles/PaletteStyles';
 
 class SingleColorPalette extends Component {
   constructor(props) {
